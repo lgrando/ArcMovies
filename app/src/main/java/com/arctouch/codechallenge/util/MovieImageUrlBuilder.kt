c6156afe3fcddf7eea730d41bda13cadb4464a6a
Lucas Grando <lucas.grando@southsystem.com.br>
2020-02-08 17:39:49 -0300

    Added movie poster and backdrop images on details

--- app/src/main/java/com/arctouch/codechallenge/util/MovieImageUrlBuilder.kt
+++
@@ -1,7 +0,0 @@
-package com.arctouch.codechallenge.util
-
-class MovieImageUrlBuilder {
-
-    fun buildPosterUrl(posterPath: String) = "$POSTER_URL$posterPath?api_key=$API_KEY"
-    fun buildBackdropUrl(backdropPath: String) = "$BACKDROP_URL$backdropPath?api_key=$API_KEY"
-}
